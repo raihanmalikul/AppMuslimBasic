931cc03192def163953da1d7fdc409a7b405430b
raihanmalikul <raihanmalikulmulki@gmail.com>
2022-07-28 18:10:13 +0700

    mengisi tabel m_produk

--- app/Database/Seeds/MProductSeeds.php
+++ app/Database/Seeds/MProductSeeds.php
@@ -3,7 +3,6 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\I18n\Time;
-
 use CodeIgniter\Database\Seeder;
 
 class MProductSeeds extends Seeder
@@ -11,18 +10,396 @@ class MProductSeeds extends Seeder
     public function run()
     {
         $data = [
-            'name'          => 'BUTTON PANTS',
-            'slug'          => '',
-            'category_id'   => 3,
-            'description'   => '',
-            'image'         => '1.jpg',
-            'price'         => 189000,
-            'weight'        => '0.3 Kg',
-            'stock'         => 100,
-            'created_at'    => TIme::now()
+            [
+                'name'          => 'BUTTON PANTS',
+                'slug'          => '',
+                'category_id'   => 3,
+                'description'   => '',
+                'image'         => '1.jpg',
+                'price'         => 189000,
+                'weight'        => 300,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'CHINO PANTS',
+                'slug'          => '',
+                'category_id'   => 3,
+                'description'   => '',
+                'image'         => '2.jpg',
+                'price'         => 199000,
+                'weight'        => 300,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'DEAN CULOTTE',
+                'slug'          => '',
+                'category_id'   => 3,
+                'description'   => '',
+                'image'         => '3.jpg',
+                'price'         => 189000,
+                'weight'        => 300,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'FLARE PANTS',
+                'slug'          => '',
+                'category_id'   => 3,
+                'description'   => '',
+                'image'         => '4.jpg',
+                'price'         => 179000,
+                'weight'        => 300,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'GEMMA PANTS',
+                'slug'          => '',
+                'category_id'   => 3,
+                'description'   => '',
+                'image'         => '5.jpg',
+                'price'         => 179000,
+                'weight'        => 300,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'INNER PENTS',
+                'slug'          => '',
+                'category_id'   => 3,
+                'description'   => '',
+                'image'         => '6.jpg',
+                'price'         => 89000,
+                'weight'        => 300,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'HIGH WAIST CULOTTE',
+                'slug'          => '',
+                'category_id'   => 3,
+                'description'   => '',
+                'image'         => '7.jpg',
+                'price'         => 189000,
+                'weight'        => 300,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'KNIT JOGGER PANTS',
+                'slug'          => '',
+                'category_id'   => 3,
+                'description'   => '',
+                'image'         => '8.jpg',
+                'price'         => 199000,
+                'weight'        => 300,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'LINEA PANTS',
+                'slug'          => '',
+                'category_id'   => 3,
+                'description'   => '',
+                'image'         => '9.jpg',
+                'price'         => 189000,
+                'weight'        => 300,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'WIDE PANTS',
+                'slug'          => '',
+                'category_id'   => 3,
+                'description'   => '',
+                'image'         => '10.jpg',
+                'price'         => 199000,
+                'weight'        => 300,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'ALI SERIS',
+                'slug'          => '',
+                'category_id'   => 2,
+                'description'   => '',
+                'image'         => '11.jpg',
+                'price'         => 159000,
+                'weight'        => 350,
+                'stock'         => 50,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'BUKHORI SERIS',
+                'slug'          => '',
+                'category_id'   => 2,
+                'description'   => '',
+                'image'         => '12.jpg',
+                'price'         => 150000,
+                'weight'        => 350,
+                'stock'         => 50,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'ANDREW SMITH LENGAN PANJANG ',
+                'slug'          => '',
+                'category_id'   => 2,
+                'description'   => '',
+                'image'         => '13.jpg',
+                'price'         => 200000,
+                'weight'        => 350,
+                'stock'         => 50,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'ANDREW SMITH LENGAN PENDEK ',
+                'slug'          => '',
+                'category_id'   => 2,
+                'description'   => '',
+                'image'         => '14.jpg',
+                'price'         => 180000,
+                'weight'        => 350,
+                'stock'         => 50,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'CARDINAL JEANS',
+                'slug'          => '',
+                'category_id'   => 2,
+                'description'   => '',
+                'image'         => '15.jpg',
+                'price'         => 200000,
+                'weight'        => 350,
+                'stock'         => 50,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'CARDINAL CJ',
+                'slug'          => '',
+                'category_id'   => 2,
+                'description'   => '',
+                'image'         => '16.jpg',
+                'price'         => 200000,
+                'weight'        => 350,
+                'stock'         => 50,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'CARDINAL KEMEJA',
+                'slug'          => '',
+                'category_id'   => 2,
+                'description'   => '',
+                'image'         => '17.jpg',
+                'price'         => 200000,
+                'weight'        => 350,
+                'stock'         => 50,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'CARDINAL  CCSL',
+                'slug'          => '',
+                'category_id'   => 2,
+                'description'   => '',
+                'image'         => '18.jpg',
+                'price'         => 210000,
+                'weight'        => 350,
+                'stock'         => 50,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'ANDREW SMITH KEMEJA',
+                'slug'          => '',
+                'category_id'   => 2,
+                'description'   => '',
+                'image'         => '19.jpg',
+                'price'         => 210000,
+                'weight'        => 350,
+                'stock'         => 50,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'ANDREW SMITH',
+                'slug'          => '',
+                'category_id'   => 2,
+                'description'   => '',
+                'image'         => '20.jpg',
+                'price'         => 200000,
+                'weight'        => 350,
+                'stock'         => 50,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'THOBE PREMIUM',
+                'slug'          => '',
+                'category_id'   => 1,
+                'description'   => '',
+                'image'         => '21.jpg',
+                'price'         => 289000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'SALMAN SERIS',
+                'slug'          => '',
+                'category_id'   => 1,
+                'description'   => '',
+                'image'         => '22.jpg',
+                'price'         => 200000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'THOBE SACHET',
+                'slug'          => '',
+                'category_id'   => 1,
+                'description'   => '',
+                'image'         => '23.jpg',
+                'price'         => 250000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'NOOR ABAY',
+                'slug'          => '',
+                'category_id'   => 1,
+                'description'   => '',
+                'image'         => '24.jpg',
+                'price'         => 160000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'BALQIS ABAYA',
+                'slug'          => '',
+                'category_id'   => 1,
+                'description'   => '',
+                'image'         => '25.jpg',
+                'price'         => 160000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'NONA ABAYA',
+                'slug'          => '',
+                'category_id'   => 1,
+                'description'   => '',
+                'image'         => '26.jpg',
+                'price'         => 160000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'jobb',
+                'slug'          => '',
+                'category_id'   => 1,
+                'description'   => '',
+                'image'         => '27.jpg',
+                'price'         => 200000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'SALT N PEPPER',
+                'slug'          => '',
+                'category_id'   => 1,
+                'description'   => '',
+                'image'         => '28.jpg',
+                'price'         => 200000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'ARJUNA WEDA',
+                'slug'          => '',
+                'category_id'   => 1,
+                'description'   => '',
+                'image'         => '29.jpg',
+                'price'         => 200000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'LOIS JEANS',
+                'slug'          => '',
+                'category_id'   => 1,
+                'description'   => '',
+                'image'         => '30.jpg',
+                'price'         => 250000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'HIJAB SERIS',
+                'slug'          => '',
+                'category_id'   => 4,
+                'description'   => '',
+                'image'         => '31.jpg',
+                'price'         => 160000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'HIJAB SERIS',
+                'slug'          => '',
+                'category_id'   => 4,
+                'description'   => '',
+                'image'         => '32.jpg',
+                'price'         => 175000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'HIJAB SERIS',
+                'slug'          => '',
+                'category_id'   => 4,
+                'description'   => '',
+                'image'         => '33.jpg',
+                'price'         => 160000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'HIJAB SERIS',
+                'slug'          => '',
+                'category_id'   => 4,
+                'description'   => '',
+                'image'         => '34.jpg',
+                'price'         => 160000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ],
+            [
+                'name'          => 'HIJAB SERIS',
+                'slug'          => '',
+                'category_id'   => 4,
+                'description'   => '',
+                'image'         => '34.jpg',
+                'price'         => 160000,
+                'weight'        => 500,
+                'stock'         => 100,
+                'created_at'    => TIme::now()
+            ]
+
+
         ];
 
         // Using Query Builder
-        $this->db->table('m_product')->insert($data);
+        $this->db->table('m_product')->insertBatch($data);
     }
 }
